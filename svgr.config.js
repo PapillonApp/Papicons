dee010279455043b726303ede0cba00b7a0b181d
godetremy <godetremy@free.fr>
2025-08-12 22:03:07 +0200

    feat: Remove XMLNS in svg

--- svgr.config.js
+++ svgr.config.js
@@ -22,6 +22,12 @@ module.exports = {
           attrs: '(fill|stroke)',
         },
       },
+      {
+        name: 'removeXMLNS',
+        params: {
+          xmlns: true,
+        },
+      }
     ],
   }
 };
