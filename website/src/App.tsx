6d66cc5ef1190592163843e742d5af5e547ce8de
Vince Linise <contact@vincelinise.com>
2026-03-27 15:26:32 +0100

    feat(website): Add footer

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -120,6 +120,19 @@ function App() {
           <IconList icons={filteredIcons} />
         </div>
 
+        <div className="flex flex-col md:flex-row items-center justify-between md:gap-4 mt-16 md:mt-0">
+          <p className="text-sm text-neutral-500">
+            © 2026 Association Papillon - This website is open source
+          </p>
+
+          <p className="text-sm text-neutral-500">
+            Made with {'<3'} by the Papillon Team -{' '}
+            <a href="https://papillon.bzh" className="hover:underline">
+              papillon.bzh
+            </a>
+          </p>
+        </div>
+
         <Toaster />
       </div>
     </main>
