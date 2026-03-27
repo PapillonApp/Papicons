6e80648befb698f790f51ae57d14b2d08a939c25
Vince Linise <contact@vincelinise.com>
2026-03-27 15:01:41 +0100

    feat: Website UI enhancements

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,5 +1,7 @@
 import { useMemo, useState } from 'react';
 import './style.css';
+import PapiconsLogotype from './icons/PapiconsLogotype';
+import toast, { Toaster } from 'react-hot-toast';
 
 type IconFile = {
   default: string;
@@ -45,11 +47,56 @@ function App() {
   }, [query]);
 
   return (
-    <main className="flex justify-center bg-neutral-100 min-h-screen">
-      <div className="w-full max-w-5xl m-16 gap-6 flex flex-col">
+    <main className="flex justify-center bg-[#f0f2f9] min-h-screen">
+      <div className="w-full max-w-5xl m-8 md:m-16 gap-2 md:gap-6 flex flex-col">
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-8 md:gap-12 self-start w-full">
+          <PapiconsLogotype className="w-42" />
+
+          <ul className="flex items-center md:justify-end gap-6 md:gap-8 text-lg text-neutral-700">
+            <li className="font-medium text-neutral-900 hover:text-blue-700">
+              <a href="https://papillon.bzh">The Papillon Project</a>
+            </li>
+            <li className="font-medium text-neutral-900 hover:text-blue-700">
+              <a href="https://docs.papillon.bzh/papicons">Docs</a>
+            </li>
+            <li className="font-medium text-neutral-900 hover:text-blue-700">
+              <a href="https://github.com/PapillonApp/Papicons">GitHub</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center gap-10 my-6">
+          <div className="max-w-3xl flex items-start gap-5 justify-center flex-col">
+            <h1 className="text-5xl tracking-tight font-bold text-left">
+              Open source, simple, human icons for everyone.
+            </h1>
+            <p className="text-xl font-medium text-left text-neutral-700">
+              Papicons is a free and open source icon library designed for
+              everyone, focused on simplicity and accessibility.
+            </p>
+            <a
+              href="https://github.com/PapillonApp/Papicons/blob/react-native/LICENSE"
+              className="text-md -mt-1 leading-relaxed text-left text-neutral-400 hover:underline"
+            >
+              Papicons is an open-source project under the MIT license.{' '}
+            </a>
+          </div>
+
+          <iframe
+            className="aspect-video rounded-xl w-full max-w-120 shadow-lg -mt-2 hidden lg:block"
+            src="https://www.youtube.com/embed/ob53CHx9kTs?si=zrLPHiaoKY3noJWm&amp;controls=0"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          />
+        </div>
+
         <SearchInput value={query} onChange={(e) => setQuery(e.target.value)} />
 
         <IconList icons={filteredIcons} />
+
+        <Toaster />
       </div>
     </main>
   );
@@ -63,10 +110,10 @@ const SearchInput = ({
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <div className="flex items-center gap-2 bg-neutral-200 rounded-full transition-all">
+    <div className="flex items-center gap-2 bg-[#e1e4ee] rounded-full transition-all mb-6 md:mb-0">
       <input
         type="search"
-        placeholder="Search icons"
+        placeholder="Search for an icon"
         value={value}
         onChange={onChange}
         className="w-full px-7 h-15 text-xl bg-transparent focus:outline-none font-medium"
@@ -89,7 +136,9 @@ const IconItem = ({ icon }: { icon: Icon }) => {
   const handleCopy = async () => {
     try {
       await navigator.clipboard.writeText(icon.svg);
+      toast.success('SVG copied to clipboard!');
     } catch {
+      toast.error('Failed to copy SVG to clipboard.');
       return;
     }
   };
@@ -98,11 +147,13 @@ const IconItem = ({ icon }: { icon: Icon }) => {
     <button
       type="button"
       onClick={handleCopy}
-      className="icon-button group aspect-square flex items-center justify-center rounded-2xl bg-white hover:shadow-lg transition-all cursor-pointer hover:scale-110 border border-neutral-100 hover:border-neutral-400 active:shadow-md active:scale-100"
+      className="icon-button group aspect-square flex items-center justify-center overflow-visible rounded-2xl bg-white hover:shadow-lg transition-all cursor-pointer hover:scale-110 border border-neutral-100 hover:border-neutral-400 active:shadow-md active:scale-100"
       aria-label={`Copy ${icon.name} SVG`}
-      title={`Copy ${icon.name} SVG`}
     >
       <IconRenderer src={icon.src} />
+      <span className="pointer-events-none absolute -top-2 left-1/2 z-100 w-max -translate-x-1/2  whitespace-nowrap rounded-full bg-neutral-900 px-2 py-1 text-sm font-medium leading-none text-white opacity-0 transition-all group-hover:opacity-100 group-focus-visible:opacity-100 group-hover:delay-50 duration-200 scale-80 group-hover:scale-100 -translate-y-4 group-hover:-translate-y-full">
+        {icon.name}
+      </span>
     </button>
   );
 };
