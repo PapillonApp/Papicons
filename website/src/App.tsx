cf4c51ae7ef2384d40a7a434f5060f8134043ad2
Vince Linise <contact@vincelinise.com>
2026-03-28 15:43:32 +0100

    chore: Different copy modes

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,6 +3,7 @@ import './style.css';
 import PapiconsLogotype from './icons/PapiconsLogotype';
 import toast, { Toaster } from 'react-hot-toast';
 import { AnimatePresence, motion } from 'motion/react';
+import { Papicons } from '@getpapillon/papicons';
 
 type IconFile = {
   default: string;
@@ -19,6 +20,13 @@ const iconSource = import.meta.glob('../../icons/*.svg', {
   eager: true,
   as: 'raw',
 });
+
+const formatHyphenatedName = (name: string) => {
+  return name
+    .split('-')
+    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+    .join('');
+};
 
 const icons = Object.entries(iconModules)
   .map(([path, module]) => {
@@ -65,6 +73,8 @@ function App() {
       return queryTokens.every((token) => normalizedName.includes(token));
     });
   }, [query]);
+
+  const [copyMode, setCopyMode] = useState<CopyModeTypes>('svg');
 
   return (
     <main className="flex justify-center bg-[#f0f2f9] min-h-screen">
@@ -113,11 +123,15 @@ function App() {
         </div>
 
         <div className="min-h-screen gap-4 flex flex-col">
-          <SearchInput
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-          />
-          <IconList icons={filteredIcons} />
+          <div className="flex items-center gap-4">
+            <SearchInput
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <CopyModeSelector mode={copyMode} onChange={setCopyMode} />
+          </div>
+
+          <IconList icons={filteredIcons} copyMode={copyMode} />
         </div>
 
         <div className="flex flex-col md:flex-row items-center justify-between md:gap-4 mt-16 md:mt-0">
@@ -133,7 +147,18 @@ function App() {
           </p>
         </div>
 
-        <Toaster />
+        <Toaster
+          toastOptions={{
+            className: '',
+            style: {
+              borderRadius: '80px',
+              fontWeight: '500',
+              padding: '6px 12px',
+              paddingRight: '8px',
+              boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+            },
+          }}
+        />
       </div>
     </main>
   );
@@ -147,19 +172,101 @@ const SearchInput = ({
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <div className="flex items-center gap-2 bg-[#e1e4ee] rounded-full transition-all mb-6 md:mb-0">
+    <div className="flex items-center gap-4 bg-[#e1e4ee] rounded-full transition-all mb-6 md:mb-0 px-5 w-full">
+      <div className="w-6 opacity-60">
+        <Papicons name="search" size={28} />
+      </div>
+
       <input
         type="search"
         placeholder="Search for an icon"
         value={value}
         onChange={onChange}
-        className="w-full px-7 h-15 text-xl bg-transparent focus:outline-none font-medium"
+        className="w-full h-15 text-xl bg-transparent focus:outline-none font-medium"
       />
+
+      <AnimatePresence mode="popLayout" initial={false}>
+        {value.length > 0 && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.7 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.7 }}
+          >
+            <motion.button
+              type="button"
+              onClick={() => onChange({ target: { value: '' } } as any)}
+              className="p-2 rounded-full hover:bg-neutral-300 transition-all"
+              aria-label="Clear search input"
+            >
+              <Papicons name="cross" />
+            </motion.button>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
-const IconList = ({ icons }: { icons: Icon[] }) => {
+type CopyModeTypes = 'svg' | 'jsx' | 'name' | 'react';
+
+const copyModes = [
+  { value: 'svg', label: 'SVG', icon: 'code' },
+  { value: 'jsx', label: 'JSX', icon: 'code' },
+  { value: 'name', label: 'Icon name', icon: 'pen' },
+  { value: 'react', label: 'React', icon: 'grid' }
+];
+
+const CopyModeSelector = ({
+  mode,
+  onChange,
+}: {
+  mode: CopyModeTypes;
+  onChange: (mode: CopyModeTypes) => void;
+}) => {
+  return (
+    <div className="w-100 flex items-center gap-4 bg-[#e1e4ee] rounded-full transition-all mb-6 md:mb-0 px-5">
+      <AnimatePresence mode="popLayout" initial={false}>
+        <motion.div
+          className="w-6 opacity-60"
+          key={mode}
+          initial={{ opacity: 0, scale: 0.6 }}
+          animate={{ opacity: 1, scale: 1 }}
+          exit={{ opacity: 0, scale: 0.6 }}
+        >
+          <Papicons
+            name={copyModes.find((m) => m.value === mode)?.icon || 'code'}
+            className="opacity-60"
+            size={28}
+          />
+        </motion.div>
+      </AnimatePresence>
+
+      <select
+        value={mode}
+        onChange={(e) =>
+          onChange(
+            e.target.value as 'svg' | 'jsx' | 'name' | 'react' | 'react-native'
+          )
+        }
+        className="w-full h-15 text-xl bg-transparent focus:outline-none font-medium"
+      >
+        {copyModes.map((copyMode) => (
+          <option key={copyMode.value} value={copyMode.value}>
+            {copyMode.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const IconList = ({
+  icons,
+  copyMode,
+}: {
+  icons: Icon[];
+  copyMode: CopyModeTypes;
+}) => {
   return (
     <motion.div
       layout
@@ -167,22 +274,45 @@ const IconList = ({ icons }: { icons: Icon[] }) => {
     >
       <AnimatePresence mode="sync" initial={false}>
         {icons.map((icon) => (
-          <IconItem key={icon.name} icon={icon} />
+          <IconItem key={icon.name} icon={icon} copyMode={copyMode} />
         ))}
       </AnimatePresence>
     </motion.div>
   );
 };
 
-const IconItem = ({ icon }: { icon: Icon }) => {
-  const handleCopy = async () => {
-    try {
-      await navigator.clipboard.writeText(icon.svg);
-      toast.success('SVG copied to clipboard!');
-    } catch {
-      toast.error('Failed to copy SVG to clipboard.');
-      return;
+const IconItem = ({
+  icon,
+  copyMode,
+}: {
+  icon: Icon;
+  copyMode: CopyModeTypes;
+}) => {
+  const copyIcon = useMemo(() => {
+    switch (copyMode) {
+      case 'svg':
+        return icon.svg;
+      case 'jsx':
+        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">${icon.svg
+          .replace(/<\?xml.*?\?>/, '')
+          .replace(/<!DOCTYPE.*?>/, '')
+          .replace(/<svg[^>]*>/, '')
+          .replace(/<\/svg>/, '')}</svg>`;
+      case 'name':
+        return icon.name;
+      case 'react':
+        return `<Papicons name="${formatHyphenatedName(icon.name)}" />`;
+      default:
+        return icon.svg;
     }
+  }, [icon, copyMode]);
+
+  const handleCopy = async () => {
+    toast.promise(navigator.clipboard.writeText(copyIcon), {
+      loading: `Copying ${copyMode === 'svg' ? 'SVG' : copyMode === 'jsx' ? 'JSX' : copyMode === 'name' ? 'Name' : copyMode === 'react' ? 'React' : 'React Native'} to clipboard...`,
+      success: `${copyMode === 'svg' ? 'SVG' : copyMode === 'jsx' ? 'JSX' : copyMode === 'name' ? icon.name : copyMode === 'react' ? 'React' : 'React Native'} copied to clipboard!`,
+      error: 'Failed to copy to clipboard.',
+    });
   };
 
   return (
@@ -202,9 +332,9 @@ const IconItem = ({ icon }: { icon: Icon }) => {
         type="button"
         onClick={handleCopy}
         className="w-full h-full icon-button group aspect-square flex items-center justify-center overflow-visible rounded-2xl bg-white hover:shadow-lg transition-all cursor-pointer hover:scale-110 border border-neutral-100 hover:border-neutral-400 active:shadow-md active:scale-100"
-        aria-label={`Copy ${icon.name} SVG`}
+        aria-label={`Copy ${icon.name} ${copyMode === 'svg' ? 'SVG' : copyMode === 'jsx' ? 'JSX' : copyMode === 'name' ? 'Name' : copyMode === 'react' ? 'React' : 'React Native'}`}
       >
-        <IconRenderer src={icon.src} />
+        <IconRenderer name={icon.name} />
         <span className="pointer-events-none absolute -top-2 left-1/2 z-100 w-max -translate-x-1/2  whitespace-nowrap rounded-full bg-neutral-900 px-2 py-1 text-sm font-medium leading-none text-white opacity-0 transition-all group-hover:opacity-100 group-focus-visible:opacity-100 group-hover:delay-50 duration-200 scale-80 group-hover:scale-100 -translate-y-4 group-hover:-translate-y-full">
           {icon.name}
         </span>
@@ -213,13 +343,11 @@ const IconItem = ({ icon }: { icon: Icon }) => {
   );
 };
 
-const IconRenderer = ({ src }: { src: string }) => {
+const IconRenderer = ({ name }: { name: string }) => {
   return (
-    <img
-      src={src}
-      alt="icon"
-      className="w-9 aspect-square opacity-70 group-hover:opacity-100 group-hover:w-10 transition-all group-active:w-9"
-    />
+    <div className="aspect-square opacity-70 group-hover:opacity-100 transition-all">
+      <Papicons color="#071833" size={32} name={formatHyphenatedName(name)} />
+    </div>
   );
 };
 
